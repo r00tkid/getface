c636b5131fe93a46fde45491e1392df9e5567554
GetFace <getface.development@gmail.com>
2019-01-17 14:05:48 +0000

    [Agenda] create actions, think serializers

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
+from agenda.views import agenda, face
 
+urlpatterns = [
+    path('/agenda', agenda.AgendaActions.as_view()),
+    path('/time', face.FaceTimeActions.as_view()),
 ]
