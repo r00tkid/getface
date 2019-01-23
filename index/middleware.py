be06f254414e7a0deceb395f2a79645fa84b7b24
GetFace <getface.development@gmail.com>
2019-01-23 09:00:45 +0000

    [Server] fix

--- index/middleware.py
+++ index/middleware.py
@@ -14,7 +14,7 @@ class SimpleMiddleware:
                 },
                 status=200,
                 headers={
-                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Origin": "http://[::1]:9090, http://localhost:9090, http://127.0.0.1:9090, http://0.0.0.0:9090, http://139.59.211.27:9090",
                     "Access-Control-Allow-Methods": "GET, OPTIONS, POST, DELETE, PUT, PURGE",
                     "Access-Control-Allow-Headers": "Content-type, Authorization, X-Requested-With, X-XSRF-TOKEN",
                     "Access-Control-Allow-Credentials": True,
